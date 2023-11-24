5cd7f4abaeddbb247cbdc8ab6110c5ee48426f17
ata_turkoglu <ata.turkoglu@reengen.com>
2023-11-24 23:43:08 +0300

    add category reducer

--- src/store/reducers/products.ts
+++ src/store/reducers/products.ts
@@ -1,4 +1,10 @@
-import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import {
+    createSlice,
+    createAsyncThunk,
+    PayloadAction,
+    createSelector,
+} from "@reduxjs/toolkit";
+import { RootState } from "../store";
 import axios from "axios";
 
 const api = "https://fakestoreapi.com";
@@ -18,22 +24,36 @@ export interface Product {
 
 export interface Products {
     products: Product[];
+    categories: String[];
 }
 
 const initialState: Products = {
     products: [],
+    categories: [],
 };
 
 export const productsSlice = createSlice({
     name: "products",
     initialState,
-    reducers: {},
+    reducers: {
+        setCategories: (state, action) => {
+            action.payload.forEach((item: Product) => {
+                let found = state.categories.find(
+                    (cat) => cat == item.category
+                );
+                if (!found) {
+                    state.categories.push(item.category);
+                }
+            });
+        },
+    },
     extraReducers: (builder) => {
         builder
             .addCase(
                 fetchProducts.fulfilled,
                 (state, action: PayloadAction<any>) => {
                     state.products = [...action.payload];
+                    productsSlice.caseReducers.setCategories(state, action);
                 }
             )
             .addCase(fetchProducts.rejected, () => {
@@ -50,6 +70,11 @@ export const fetchProducts = createAsyncThunk(
         });
         return response;
     }
+);
+
+export const getCategories = createSelector(
+    [(state: RootState) => state.productsSlice.products],
+    (items) => items.length
 );
 
 export default productsSlice.reducer;
